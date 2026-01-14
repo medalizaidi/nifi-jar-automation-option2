b60c161196a7a7f4390abd197608b961d57b4987
medalizaidi <143266679+medalizaidi@users.noreply.github.com>
2026-01-14 01:38:21 +0100

    Update NiFi image to 814e81b

--- workload/mfx-aggre-data-platform/ecs_task_definition.tf
+++ workload/mfx-aggre-data-platform/ecs_task_definition.tf
@@ -15,7 +15,7 @@ resource "aws_ecs_task_definition" "nifi" {
   container_definitions = jsonencode([
     {
       "name" : "nifi",
-      "image" : "478874601698.dkr.ecr.ap-northeast-1.amazonaws.com/apache-nifi-with-custom-jars-repo:b9b277c",
+      "image" : "478874601698.dkr.ecr.ap-northeast-1.amazonaws.com/apache-nifi-with-custom-jars-repo:814e81b",
       "essential" : true,
 
       "cpu" : 2048,
